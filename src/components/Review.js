08d6a24321c6029efa54b877489199b075054dee
Betty Chow <bettychchow@gmail.com>
2018-05-14 16:47:53 -0700

    Add Facebook share button

--- src/components/Review.js
+++ src/components/Review.js
@@ -10,6 +10,7 @@ import {
   Redirect
 } from 'react-router-dom'
 import jwtDecode from'jwt-decode'
+import DocumentMeta from 'react-document-meta'
 import { getCurrentReview, vote, getRestaurant, deleteReview } from '../actions/index'
 import ReviewForm from './ReviewForm';
 
@@ -22,6 +23,25 @@ class Review extends Component {
     console.log('reviewid in Review', this.props.match.params.reviewid)
     this.props.getCurrentReview(this.props.match.params.reviewid)
     this.props.getRestaurant(this.props.match.params.restaurant_id)
+    const FB = window.FB;
+    window.fbAsyncInit = function() {
+      //SDK loaded, initialize it
+      FB.init({
+          appId      : '207666283375899',
+          xfbml      : true,
+          version    : 'v2.6'
+      });
+      //JS SDK initialized, now you can use it
+      FB.XFBML.parse();
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0&appId=207666283375899';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'))
  
   }
 
@@ -45,6 +65,20 @@ class Review extends Component {
   //this.toggleEdit = this.toggleEdit.bind(this)
 
   render() {
+
+    const meta = {
+      title: 'Some Meta Title',
+      description: 'I am a description, and I can create multiple tags',
+      canonical: 'https://gentle-taiga-80518.herokuapp.com/review/Gochi%20Japanese%20Fusion%20Tapas/bettychow/1/1',
+      meta: {
+        charset: 'utf-8',
+        name: {
+          keywords: 'react,meta,document,html,tags'
+        }
+      }
+    };
+
+
     this.toggleEdit = this.toggleEdit.bind(this)
     const token = localStorage.getItem('authorization')
     const { reviewid , username } = this.props.match.params
@@ -81,33 +115,38 @@ class Review extends Component {
       )
     } else {
       return(
-        <div>
-          <Link to={`/allreviews/${currentUsername}/${currentRestaurant.id}`}>See all reviews of {currentRestaurant.restaurant_name}</Link>
-          <Link to={`/${currentUsername}`}>Back to Home</Link>
-          <h3>{currentRestaurant.restaurant_name}</h3>
-          <p>{currentRestaurant.address}</p>
-          <p>{currentRestaurant.phone}</p>
-          <div className="review-body">
-            <h4 className="review-title">{currentReview.title}</h4>
-            <p>{currentReview.comment}</p>
-            <h5>Recommended Dishes</h5>
-            <p>{currentReview.dishes}</p>
-            <h5>Food Rating</h5>
-            <p>{currentReview.food_rating}</p>
-            <h5>Service Rating</h5>
-            <p>{currentReview.service_rating}</p>
-            <img className="food-image" src={currentReview.pic_01}/>
-            <img className="food-image" src={currentReview.pic_02}/>
-            <img className="food-image" src={currentReview.pic_03}/>
-            <img className="food-image" src={currentReview.pic_04}/>
-            {displayEditButton}
-            {displayDeleteButton}           
+        <DocumentMeta {...meta}>
+        
+          <div>
+            <Link to={`/allreviews/${currentUsername}/${currentRestaurant.id}`}>See all reviews of {currentRestaurant.restaurant_name}</Link>
+            <Link to={`/${currentUsername}`}>Back to Home</Link>
+            <h3>{currentRestaurant.restaurant_name}</h3>
+            <p>{currentRestaurant.address}</p>
+            <p>{currentRestaurant.phone}</p>
+            <div className="review-body">
+              <h4 className="review-title">{currentReview.title}</h4>
+              <p>{currentReview.comment}</p>
+              <h5>Recommended Dishes</h5>
+              <p>{currentReview.dishes}</p>
+              <h5>Food Rating</h5>
+              <p>{currentReview.food_rating}</p>
+              <h5>Service Rating</h5>
+              <p>{currentReview.service_rating}</p>
+              <img className="food-image" src={currentReview.pic_01}/>
+              <img className="food-image" src={currentReview.pic_02}/>
+              <img className="food-image" src={currentReview.pic_03}/>
+              <img className="food-image" src={currentReview.pic_04}/>
+              {displayEditButton}
+              {displayDeleteButton}           
+            </div>
+            <h5>Votes</h5>
+            <span>{currentReview.votes}</span>
+            {displayUpVoteButton}
+            {displayDownVoteButton}
+            <div id="fb-root"></div>
+            <div class="fb-share-button" data-href="https://gentle-taiga-80518.herokuapp.com/review/Gochi%20Japanese%20Fusion%20Tapas/bettychow/1/1" data-layout="button" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fgentle-taiga-80518.herokuapp.com%2Freview%2FGochi%2520Japanese%2520Fusion%2520Tapas%2Fbettychow%2F1%2F1&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
           </div>
-          <h5>Votes</h5>
-          <span>{currentReview.votes}</span>
-          {displayUpVoteButton}
-          {displayDownVoteButton}
-        </div>
+        </DocumentMeta>
       )
     }
   }
